585ceaeecd6041120ee14c19e5c5d764a2dff77d
rshuken <ryanshuken@gmail.com>
2020-12-30 14:00:18 -0700

    done and working on server and vercel

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-
+// I had issues with this but it is now working after I declared it with a const and exported it destructed.
 const REACT_APP_API_BASE = process.env.REACT_APP_API_BASE || 'http://localhost:8000/'
 
 export {REACT_APP_API_BASE};
